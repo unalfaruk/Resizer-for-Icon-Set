154e37c5bf0d8e2360ed7fd24a7ac3e9343937df
Faruk UNAL <faruk@debian>
2017-03-17 22:26:38 +0300

    Add warning system for finished operation.

--- ButtonFunction.py
+++ ButtonFunction.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 from tkinter import *
+import tkinter.messagebox
 from tkinter.filedialog import askopenfilename
 from PIL import Image,ImageTk
-import os
+import os, sys
 
 root= Tk()
 root.title("Resizer for Icon Set")
@@ -15,22 +17,27 @@ def showImage(filename):
     label.configure(image=photo)
     label.image=photo
 
-
-def printName():
-    print("Selam")
-
 def convert():
+    error=0
     if(filename!=""):
-        print("Nice!")
+        #print("Nice!")
         newWidth=256
         newHeight=256
-        print(type(newWidth))
+        #print(type(newWidth))
         for size in range(1,6):
             img=Image.open(filename)
             newWidth=newWidth/2
             newHeight=newHeight/2
-            img=img.resize((int(newWidth),int(newHeight)), Image.ANTIALIAS)
-            img.save(os.path.split(filename)[0]+"/"+str(int(newWidth))+"_"+os.path.split(filename)[1])
+            try:
+                img=img.resize((int(newWidth),int(newHeight)), Image.ANTIALIAS)
+                img.save(os.path.split(filename)[0]+"/"+str(int(newWidth))+"_"+os.path.split(filename)[1])
+            except:
+                error=error+1
+
+        if(error==0):
+            tkinter.messagebox.showinfo("Operation Completed!", "Congrulation! Successfull.")
+        elif(error>0):
+            tkinter.messagebox.showinfo("Operation Completed!", "Ooops! Not successfull, there is an error.")
 
     else:
         print("Not work!")
@@ -38,12 +45,22 @@ def convert():
 def chooseAFile():
     global filename
     filename = askopenfilename()
-    if(filename.find(".jpg") != -1 or filename.find(".png") != -1 or filename.find(".gif") != -1):
-        showImage(filename)
-    else:
-        print("Please, select an image file!")
+    checkMime(filename)
+
+def checkMime(path):
+    if(type(path)==str):
+        if(filename.find(".jpg") != -1 or filename.find(".png") != -1 or filename.find(".gif") != -1):
+            showImage(filename)
+        else:
+            print("Please, select an image file!")
 
 frameImage= Frame(root)
+
+image=PhotoImage()
+label=Label(frameImage,image=None)
+label.grid(row=0,column=0)
+
+
 frameButtons = Frame(root)
 
 buttonBrowse = Button(frameButtons, text="Browse", command=chooseAFile)
@@ -52,12 +69,12 @@ buttonBrowse.grid(row=0,column=0,padx=5,pady=5)
 buttonConvert= Button(frameButtons, text="Convert", command=convert)
 buttonConvert.grid(row=0,column=1,padx=5, pady=5)
 
-image=PhotoImage()
-label=Label(frameImage,image=None)
-label.grid(row=0,column=0)
 
 frameImage.pack(side=TOP,fill=X)
-frameButtons.pack(side=BOTTOM)
-#frame.grid(row=0,column=1)
+frameButtons.pack(side=BOTTOM, fill=X)
+
+if len(sys.argv)>1:
+    filename=sys.argv[1]
+    checkMime(filename)
 
 root.mainloop()

ea74bbb72c0c224a22533d958c1a82aca314fc70
Faruk UNAL <faruk@debian>
2017-03-17 17:41:36 +0300

    Add Convert button and function

--- ButtonFunction.py
+++ ButtonFunction.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from PIL import Image,ImageTk
+import os
 
 root= Tk()
-root.title("Faruk")
+root.title("Resizer for Icon Set")
+filename=""
 
 def showImage(filename):
-    print(filename)
+    #print(filename)
     image = Image.open(filename)
-    print(type(image))
+    #print(type(image))
     photo = ImageTk.PhotoImage(image)
     label.configure(image=photo)
     label.image=photo
@@ -17,10 +19,29 @@ def showImage(filename):
 def printName():
     print("Selam")
 
-def chooseAFile():
-    filename = askopenfilename()
-    showImage(filename)
+def convert():
+    if(filename!=""):
+        print("Nice!")
+        newWidth=256
+        newHeight=256
+        print(type(newWidth))
+        for size in range(1,6):
+            img=Image.open(filename)
+            newWidth=newWidth/2
+            newHeight=newHeight/2
+            img=img.resize((int(newWidth),int(newHeight)), Image.ANTIALIAS)
+            img.save(os.path.split(filename)[0]+"/"+str(int(newWidth))+"_"+os.path.split(filename)[1])
 
+    else:
+        print("Not work!")
+
+def chooseAFile():
+    global filename
+    filename = askopenfilename()
+    if(filename.find(".jpg") != -1 or filename.find(".png") != -1 or filename.find(".gif") != -1):
+        showImage(filename)
+    else:
+        print("Please, select an image file!")
 
 frameImage= Frame(root)
 frameButtons = Frame(root)
@@ -28,7 +49,7 @@ frameButtons = Frame(root)
 buttonBrowse = Button(frameButtons, text="Browse", command=chooseAFile)
 buttonBrowse.grid(row=0,column=0,padx=5,pady=5)
 
-buttonConvert= Button(frameButtons, text="Convert", command=printName)
+buttonConvert= Button(frameButtons, text="Convert", command=convert)
 buttonConvert.grid(row=0,column=1,padx=5, pady=5)
 
 image=PhotoImage()
